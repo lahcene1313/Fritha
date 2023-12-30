afefb4f1639a9a8b41aebe1cacbf7ec2d2bec4bc
lahcene1313 <44542038+lahcene1313@users.noreply.github.com>
2023-12-30 11:56:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@
 
             const menuItems = [
                 { plat: 'Pizza au feu de bois', Prix: 1300.00},
-				{ plat: 'Soupe de Lentilles', Prix: 350.00},
+		{ plat: 'Soupe de Lentilles', Prix: 350.00},
                 { plat: 'Houmous', Prix: 500.00 },
                 { plat: 'Yogurtlu Salatasi', Prix: 550.00},
                 { plat: 'Burger', Prix: 1250.00},
@@ -28,7 +28,6 @@
                 menuItem.className = 'menu-card';
                 menuItem.innerHTML = `
                     <h3>${item.plat}</h3>
-                   
                     <p>Prix: ${item.Prix.toFixed(2)} DA</p>
                     <button class="button-Ajouter" onclick="addToCart('${item.plat}', ${item.Prix})">Ajouter</button><br><br>
                     <input type="number" value="1" min="1">
