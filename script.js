d29257f54a173e15bb118afe37b948253aeec1e9
lahcene1313 <44542038+lahcene1313@users.noreply.github.com>
2023-12-31 17:00:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -28,7 +28,7 @@
         menuItem.className = 'menu-card';
         menuItem.innerHTML = `
             <h3>${item.plat}</h3>
-            <p>Description: ${item.description}</p>
+            <p>${item.description}</p>
             <p>Prix: ${item.Prix.toFixed(2)} DA</p>
             <button class="button-Ajouter" onclick="addToCart('${item.plat}', ${item.Prix})">Ajouter</button><br><br>
             <input type="number" value="1" min="1">
